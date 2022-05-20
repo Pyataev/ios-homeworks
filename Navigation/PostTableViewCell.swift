326ce2a26ac54ab8a9d2f606e04a3412bf442738
Pyataev <pyataevaleksei@gmail.com>
2022-05-20 11:01:42 +0500

    домашнее задание CollectionView Final

--- Navigation/PostTableViewCell.swift
+++ Navigation/PostTableViewCell.swift
@@ -2,19 +2,19 @@
 //  PostTableViewCell.swift
 //  Navigation
 //
-//  Created by User on 03.05.2022.
+//  Created by User on 20.05.2022.
 //
 
 import UIKit
 
 final class PostTableViewCell: UITableViewCell {
-
+    
     struct ViewModel: ViewModelProtocol {
         var author, description, image: String
         var likes: Int
         var views: Int
     }
-
+    
     private lazy var backView: UIView = {
         let view = UIView()
         view.backgroundColor = .white
@@ -22,7 +22,7 @@ final class PostTableViewCell: UITableViewCell {
         view.clipsToBounds = true
         return view
     }()
-
+    
     private lazy var authorLabel: UILabel = {
         let label = UILabel()
         label.backgroundColor = .clear
@@ -34,17 +34,17 @@ final class PostTableViewCell: UITableViewCell {
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
-
+    
     private lazy var postImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.contentMode = .scaleAspectFit
         imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.backgroundColor = .black
-
+        
         return imageView
     }()
-
-
+    
+    
     private lazy var descriptionLabel: UILabel = {
         let label = UILabel()
         label.numberOfLines = 0
@@ -53,19 +53,20 @@ final class PostTableViewCell: UITableViewCell {
         label.textColor = .systemGray
         label.setContentCompressionResistancePriority(UILayoutPriority(250), for: .vertical)
         label.translatesAutoresizingMaskIntoConstraints = false
+       
         return label
     }()
-
+    
     private lazy var stackView: UIStackView = {
         let stackView = UIStackView()
         stackView.axis = .horizontal
         stackView.distribution = .fill
         stackView.setContentCompressionResistancePriority(UILayoutPriority(250), for: .vertical)
         stackView.translatesAutoresizingMaskIntoConstraints = false
-
+        
         return stackView
     }()
-
+    
     private lazy var likesLabel: UILabel = {
         let label = UILabel()
         label.text  = "Likes:"
@@ -76,10 +77,10 @@ final class PostTableViewCell: UITableViewCell {
         label.setContentHuggingPriority(UILayoutPriority(1), for: .horizontal)
         label.setContentCompressionResistancePriority(UILayoutPriority(250), for: .vertical)
         label.translatesAutoresizingMaskIntoConstraints = false
-
+        
         return label
     }()
-
+    
     private lazy var viewsLabel: UILabel = {
         let label = UILabel()
         label.text  = "Views: "
@@ -89,21 +90,21 @@ final class PostTableViewCell: UITableViewCell {
         label.textColor = .black
         label.setContentCompressionResistancePriority(UILayoutPriority(250), for: .vertical)
         label.translatesAutoresizingMaskIntoConstraints = false
-
+        
         return label
     }()
-
-
-
+    
+    
+    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         self.setupView()
     }
-
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
+    
     override func prepareForReuse() {
         super.prepareForReuse()
         self.authorLabel.text = nil
@@ -112,7 +113,7 @@ final class PostTableViewCell: UITableViewCell {
         self.viewsLabel.text = nil
         self.likesLabel.text = nil
     }
-
+    
     private func setupView() {
         self.contentView.backgroundColor = .white
         self.contentView.addSubview(self.backView)
@@ -122,85 +123,77 @@ final class PostTableViewCell: UITableViewCell {
         self.backView.addSubview(self.descriptionLabel)
         self.stackView.addArrangedSubview(self.likesLabel)
         self.stackView.addArrangedSubview(self.viewsLabel)
-
-        let backViewConstraints = self.backViewConstraints()
-        let authorLabelConstraints = self.authorLabelConstraints()
-        let imageConstraints = self.imageConstraints()
-        let descriptionLabelConstraints = self.descriptionLabelConstraints()
-        let stackViewConstraints = self.stackViewConstraints()
-
+        
+        
         NSLayoutConstraint.activate(
-            backViewConstraints +
-            authorLabelConstraints +
-            imageConstraints +
-            descriptionLabelConstraints +
-            stackViewConstraints)
+            backViewConstraints() +
+            authorLabelConstraints() +
+            imageConstraints() +
+            descriptionLabelConstraints() +
+            stackViewConstraints()
+        )
     }
-
+    
     private func backViewConstraints() -> [NSLayoutConstraint] {
-        let topConstraint = self.backView.topAnchor.constraint(equalTo: self.contentView.topAnchor)
-        let leadingConstraint = self.backView.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor)
-        let trailingConstraint = self.backView.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor)
-        let bottomConstraint = self.backView.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor)
-
+        
         return [
-            topConstraint, leadingConstraint, trailingConstraint, bottomConstraint
+            backView.topAnchor.constraint(equalTo: self.contentView.topAnchor),
+            backView.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor),
+            backView.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor),
+            backView.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor)
         ]
     }
-
+    
     private func authorLabelConstraints() -> [NSLayoutConstraint] {
-        let topConstraint = self.authorLabel.topAnchor.constraint(equalTo: self.backView.topAnchor, constant: 16)
-        let leadingConstraint = self.authorLabel.leadingAnchor.constraint(equalTo: self.backView.leadingAnchor, constant: 16)
-        let trailingConstraint = self.authorLabel.trailingAnchor.constraint(equalTo: self.backView.trailingAnchor, constant: -16)
-
+        
         return [
-            topConstraint, leadingConstraint, trailingConstraint
+            authorLabel.topAnchor.constraint(equalTo: self.backView.topAnchor, constant: 16),
+            authorLabel.leadingAnchor.constraint(equalTo: self.backView.leadingAnchor, constant: 16),
+            authorLabel.trailingAnchor.constraint(equalTo: self.backView.trailingAnchor, constant: -16)
         ]
     }
-
+    
     private func imageConstraints() -> [NSLayoutConstraint] {
-        let topConstraint = self.postImageView.topAnchor.constraint(equalTo: self.authorLabel.bottomAnchor, constant: 12)
-        let leadingConstraint = self.postImageView.leadingAnchor.constraint(equalTo: self.backView.leadingAnchor)
-        let trailingConstraint = self.postImageView.trailingAnchor.constraint(equalTo: self.backView.trailingAnchor)
-        let widthImageView = self.postImageView.heightAnchor.constraint(equalTo: self.backView.widthAnchor, multiplier: 1.0)
-
+        
         return [
-            topConstraint, leadingConstraint, trailingConstraint, widthImageView
+            postImageView.topAnchor.constraint(equalTo: self.authorLabel.bottomAnchor, constant: 12),
+            postImageView.leadingAnchor.constraint(equalTo: self.backView.leadingAnchor),
+            postImageView.trailingAnchor.constraint(equalTo: self.backView.trailingAnchor),
+            postImageView.heightAnchor.constraint(equalTo: self.backView.widthAnchor, multiplier: 1.0)
         ]
     }
-
+    
     private func descriptionLabelConstraints() -> [NSLayoutConstraint] {
-        let topConstraint = self.descriptionLabel.topAnchor.constraint(equalTo: self.postImageView.bottomAnchor, constant: 16)
-        let leadingConstraint = self.descriptionLabel.leadingAnchor.constraint(equalTo: self.backView.leadingAnchor, constant: 16)
-        let trailingConstraint = self.descriptionLabel.trailingAnchor.constraint(equalTo: self.backView.trailingAnchor, constant: -16)
-
+        
         return [
-            topConstraint, leadingConstraint, trailingConstraint
+            descriptionLabel.topAnchor.constraint(equalTo: self.postImageView.bottomAnchor, constant: 16),
+            descriptionLabel.leadingAnchor.constraint(equalTo: self.backView.leadingAnchor, constant: 16),
+            descriptionLabel.trailingAnchor.constraint(equalTo: self.backView.trailingAnchor, constant: -16)
         ]
     }
-
+    
     private func stackViewConstraints() -> [NSLayoutConstraint] {
-        let topConstraint = self.stackView.topAnchor.constraint(equalTo: self.descriptionLabel.bottomAnchor, constant: 16)
-        let leadingConstraint = self.stackView.leadingAnchor.constraint(equalTo: self.backView.leadingAnchor, constant: 16)
-        let trailingConstraint = self.stackView.trailingAnchor.constraint(equalTo: self.backView.trailingAnchor, constant: -16)
-        let bottomConstraint = self.stackView.bottomAnchor.constraint(equalTo: self.backView.bottomAnchor, constant: -16)
-
+        
         return [
-            topConstraint, leadingConstraint, trailingConstraint, bottomConstraint
+            stackView.topAnchor.constraint(equalTo: self.descriptionLabel.bottomAnchor, constant: 16),
+            stackView.leadingAnchor.constraint(equalTo: self.backView.leadingAnchor, constant: 16),
+            stackView.trailingAnchor.constraint(equalTo: self.backView.trailingAnchor, constant: -16),
+            stackView.bottomAnchor.constraint(equalTo: self.backView.bottomAnchor, constant: -16)
         ]
     }
 }
 
-//extension PostTableViewCell: Setupable {
-//
-//    func setup(with viewModel: ViewModelProtocol) {
-//        guard let viewModel = viewModel as? ViewModel else { return }
-//
-//        self.authorLabel.text = viewModel.author
-//        self.postImageView.image = UIImage(named: viewModel.image)
-//        self.descriptionLabel.text = viewModel.description
-//        self.likesLabel.text? += String(viewModel.likes)
-//        self.viewsLabel.text? += String(viewModel.views)
-//
-//    }
-//}
+extension PostTableViewCell: Setupable {
+    
+    func setup(with viewModel: ViewModelProtocol) {
+        
+        guard let viewModel = viewModel as? ViewModel else { return }
+        
+        self.authorLabel.text = viewModel.author
+        self.postImageView.image = UIImage(named: viewModel.image)
+        self.descriptionLabel.text = viewModel.description
+        self.likesLabel.text? += String(viewModel.likes)
+        self.viewsLabel.text? += String(viewModel.views)
+        
+    }
+}
